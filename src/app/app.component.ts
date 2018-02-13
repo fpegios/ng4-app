938acac8322049b49dd677b2c6c6f7e068d7e39d
Fotis Pegios <fotispeg@gmail.com>
2018-02-13 01:14:45 +0100

    components, data-binding, event-binding, tempaltes, directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 4 App';
+
+  // declared array of months.
+  months = ["January", "Feburary", "March", "April", "May", 
+  "June", "July", "August", "September",
+  "October", "November", "December"];
+
+  isavailable = true;   //variable is set to true
+
+  myClickFunction(event) {
+    this.isavailable = false;
+  }
+  changemonths(event) {
+    alert("Changed month from the Dropdown");
+    console.log(event);
+  }
 }
