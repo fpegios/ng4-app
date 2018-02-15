98f617d3f8c18a536af74b2e4cc01eab17248b8f
Fotis Pegios <fotispeg@gmail.com>
2018-02-15 01:22:46 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -18,7 +19,13 @@ import { SqrtPipe } from './app.sqrt';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+       {
+          path: 'new-cmp',
+          component: NewCmpComponent
+       }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
